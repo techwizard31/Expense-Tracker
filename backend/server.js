8b7027b6da1b89e920d2326b2548660a3035126e
techwizard31 <istaprasad.patra@gmail.com>
2024-09-14 00:55:45 +0530

    backend fixing

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
+const https = require('https');
+const fs = require('fs');
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
+
 const userRoutes = require("./routes/authlogics");
-const expenseRoutes = require("./routes/expenselogics")
+const expenseRoutes = require("./routes/expenselogics");
 
 const app = express();
 app.use(express.json());
@@ -20,11 +23,17 @@ app.use(cors(corsOptions));
 app.use("/", userRoutes);
 app.use("/expense", expenseRoutes);
 
-app.listen(process.env.PORT, async() => {
+// Read SSL certificates
+const sslOptions = {
+  key: fs.readFileSync('/etc/letsencrypt/live/kharcha.online/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/kharcha.online/fullchain.pem')
+};
+
+https.createServer(sslOptions, app).listen(process.env.PORT, async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI)
+    await mongoose.connect(process.env.MONGO_URI);
     console.log(`connected to mongodb and listening at ${process.env.PORT}`);
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 });
